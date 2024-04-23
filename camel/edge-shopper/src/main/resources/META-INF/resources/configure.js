92452613bf10176b4d98139a11d4cdcb18b17508
Hugo Guerrero <1001939+hguerrero@users.noreply.github.com>
2024-04-22 22:05:24 -0400

    configure mqtt based on deployment
    Signed-off-by: Hugo Guerrero <1001939+hguerrero@users.noreply.github.com>

---
+++ camel/edge-shopper/src/main/resources/META-INF/resources/configure.js
@@ -0,0 +1,14 @@
+window.enableMQTT = false;
+
+const hideButtons = () => {
+  if (window.location.href.indexOf("lab2-edge") > -1) {
+    const ingestion = document.getElementById("btn-ingestion");
+    if (ingestion) ingestion.style.display = "block";
+    window.enableMQTT = true;
+  } else {
+    const ingestion = document.getElementById("btn-ingestion");
+    if (ingestion) ingestion.style.display = "none";
+  }
+};
+
+hideButtons();
